89833548d75f5de17251e3ee11cc352be4a57df1
eversley1996 <33884107+eversley1996@users.noreply.github.com>
2019-11-20 22:45:50 -0500

    Update world.php

--- world.php
+++ world.php
@@ -1,17 +1,26 @@
 <?php
+
 $host = getenv('IP');
-$username = 'eversley';
+$username = "eversley";
 $password = 'E.Francis14';
 $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country =$_GET['country'];
+$all =$_GET['all'];
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+if($all =='true') {
+	$stmt = $conn->query("SELECT * FROM countries");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+else {
+	$stmt =$conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+echo '<ul>';
+foreach ($results as $row) {
+  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . $all.'</li>';
+}
+echo '</ul>';
