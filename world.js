8c6ff53903afd8da06c1f71f47e1311d10ecae76
eversley1996 <everboy15@hotmail.com>
2019-11-21 03:39:54 +0000

    Exercise 3 completed

---
+++ world.js
@@ -0,0 +1,27 @@
+window.onload= function (){
+    loadPage();
+}
+
+let loadPage = () =>{
+  let button = document.getElementById("lookup");
+  
+  button.addEventListener("click", function(){
+      var httpRequest = new XMLHttpRequest();
+      
+      var country= document.getElementById("country").value.toLowerCase();
+      
+      let url="./world.php?country=" + country.trim();
+      
+      httpRequest.open("GET", url, true);
+      
+      httpRequest.send(null);
+      
+      httpRequest.onreadystatechange = function(){
+          if (httpRequest.readyState == 4 && httpRequest.status == 200){
+              var result = this.responseText;
+              document.getElementById("result").innerHTML = result;
+          }
+      }
+      
+  });
+}
